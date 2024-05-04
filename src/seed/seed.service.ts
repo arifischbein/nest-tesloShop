4526a47d42439a758a9888470c97641ff7e599cf
Arifischbein <ariifischbein@gmail.com>
2024-05-04 00:28:44 -0300

    modify seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,60 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
 import { initialData } from './data/seed-data';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/entities/user.entity';
+import { Repository } from 'typeorm';
+import { use } from 'passport';
 
 @Injectable()
 export class SeedService {
   
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(
+    private readonly productsService: ProductsService,
+    
+    @InjectRepository(User) 
+    private readonly userRepository: Repository<User>
+  ) {}
+
   async runSeed() {
-    await this.insertNewProducts();
+    await this.deleteTables();
+    const firstUser = await this.insertUsers()
+    await this.insertNewProducts(firstUser);
     return 'Seed executed successfully!';
   }
 
-  private async insertNewProducts() {
+  private async deleteTables() {
+    await this.productsService.deleteAllProducts();
+    
+    const queryBuilder = this.userRepository.createQueryBuilder();
+    await queryBuilder
+      .delete()
+      .where({})
+      .execute()
+  }
+
+  private async insertUsers() {
+    
+    const seedUsers = initialData.users;
+
+    const users: User[] = [];
+    seedUsers.forEach(user => {
+      users.push( this.userRepository.create(user) )
+    });
+
+    const dbUsers = await this.userRepository.save(seedUsers);
+
+    return dbUsers[0];
+  }
+
+  private async insertNewProducts(user: User) {
     await this.productsService.deleteAllProducts();
     const seedProducts = initialData.products;
     
     const insertPromises = [];
-    // seedProducts.forEach(product => {
-    //   insertPromises.push(this.productsService.create(product));
-    // })
+    seedProducts.forEach(product => {
+      insertPromises.push(this.productsService.create(product, user));
+    })
     await Promise.all(insertPromises);
     return true;
   }
