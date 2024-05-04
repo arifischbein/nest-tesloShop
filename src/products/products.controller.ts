7f57071a476380ed29e88f4d89dcfdaf1afda971
Arifischbein <ariifischbein@gmail.com>
2024-05-03 23:47:45 -0300

    auth guard en otros modulos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,8 +3,10 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
 @Controller('products')
+@Auth() //asi protejo absolutamente todas las rutas de este controlador
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
