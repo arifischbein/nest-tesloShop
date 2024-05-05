a6d7b59fe9379e7961fccca37b282990d71b09c5
Arifischbein <ariifischbein@gmail.com>
2024-05-05 12:17:39 -0300

    add swagger and basic config in main

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -63,6 +63,13 @@ export class AuthService {
     //TODO: retornar el jwt
   }
 
+  async checkAuthStatus(user: User) {
+    return {
+      ...user,
+      token: this.getJwtToken({ id: user.id })
+    };
+  }
+
   private getJwtToken(payload: JwtPayload) {
     const token = this.jwtService.sign(payload);
     return token;
